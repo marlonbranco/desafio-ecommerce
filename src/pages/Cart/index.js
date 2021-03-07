7dc149a2a60e6a4caedf43e3a2ed194730186614
Marlon Branco <marlonaparecido18@gmail.com>
2021-03-07 02:12:31 -0300

    feature: Contexto de itens do carrinho feito, listagem dos itens adicionados ao carrinho na página carrinho feita

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,4 +1,5 @@
 import React, { useContext } from "react";
+
 import { FiPlusSquare, FiMinusSquare, FiTrash } from "react-icons/fi";
 import { Link } from "react-router-dom";
 
@@ -16,13 +17,67 @@ import { CartContext } from "../../hooks/cart";
 const Cart = () => {
   const [items, setItems] = useContext(CartContext);
 
+  // const [total, setTotal] = useState();
+  // const { increment, decrement, products } = useCart();
+
+  // function handleIncrement(id) {
+  //   increment(id);
+  // }
+  // function handleDecrement(id) {
+  //   decrement(id);
+  // }
+
+  // const cartSubTotal = useMemo(() => {
+  //   const total = products.reduce((accumulator, product) => {
+  //     const productsSubTotal = product.price * product.quantity;
+
+  //     return accumulator + productsSubTotal;
+  //   }, 0);
+
+  //   return formatValue(total);
+  // }, [products]);
+
+  // const shippingCost = useMemo(() => {
+  //   const total = products.reduce((accumulator, _) => {
+  //     if (cartSubTotal > 250.0) {
+  //       return 0;
+  //     } else {
+  //       accumulator += 10;
+  //     }
+  //     return accumulator;
+  //   }, 0);
+
+  //   return formatValue(total);
+  // }, [products, cartSubTotal]);
+
+  // const totalItensInCart = useMemo(() => {
+  //   const total = products.reduce((accumulator, product) => {
+  //     const productsQuantity = product.quantity;
+
+  //     return accumulator + productsQuantity;
+  //   }, 0);
+
+  //   return total;
+  // }, [products]);
+
+  // const cartTotal = useMemo(() => {
+  //   const total = products.reduce((accumulator, product) => {
+  //     const productsTotal = cartSubTotal + shippingCost;
+
+  //     return accumulator + productsTotal;
+  //   }, 0);
+
+  //   return formatValue(total);
+  // }, [products, cartSubTotal, shippingCost]);
+
+  const cartItems = [...items];
   return (
     <>
       <Header />
       <CartContainer>
         <Title>Meu carrinho</Title>
         <ul className="items">
-          {cartItems.map((item) => (
+          {cartItems.map(({ item }) => (
             <ProductsContainer key={item.id} className="item">
               <img src={`/assets/${item.image}`} alt={item.name} />
               <h3>{item.name}</h3>
@@ -63,7 +118,7 @@ const Cart = () => {
               <h2> R$ 159,90</h2>
             </span>
           </CheckoutContainer>
-          <CheckoutButton onClick={() => console.log(items)}>
+          <CheckoutButton onClick={() => console.log(cartItems)}>
             Finalizar compra
           </CheckoutButton>
         </ul>
